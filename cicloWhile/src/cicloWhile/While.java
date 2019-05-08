34a58d236409c142dcf1c09c644798dd8b62742c
mel_pasantia_incluit <melinalbp@gmail.com>
2019-05-08 11:00:45 -0300

    prueba1

---
+++ cicloWhile/src/cicloWhile/While.java
@@ -0,0 +1,35 @@
+package cicloWhile;
+
+public class While {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 0;
+//		boolean cond=true;
+//		
+//		while (cond) {
+//			System.out.println("El valor de x es: " +x);
+//			x =x+2;
+//			
+//			if (x>8) {
+//				cond = false;
+//			}
+//		}
+//		boolean cond=false;
+//		
+//		while (cond==false) {
+//			System.out.println("El valor de x es: " +x);
+//			x =x+2;
+//			
+//			if (x>8) {
+//				cond = true;
+//			}
+//		}
+		
+		while (x<=15) {
+			System.out.println("El valor de x es: " +x);
+			x =(x*2)+3;
+		}
+	}
+}
